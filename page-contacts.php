73209a113c7aa2a1222a751cb58e4b311a983b64
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2025-03-28 05:01:53 +0700

    Rename template

--- page-contacts.php
+++ page-contacts.php
@@ -2,6 +2,6 @@
 <section class="main neuropsychology-page contacts_page">
 	<div class="consultation-overlay hidden "></div>
 	<?php get_template_part('template-parts/contacts/direction'); ?>
-	<?php get_template_part('template-parts/contacts/contacts_map'); ?>
+	<?php get_template_part('template-parts/contacts/contacts-map'); ?>
 </section>
 <?php get_footer() ?>
