84908fd74332961b2264732177f63db88eb05cff
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2025-03-31 02:08:35 +0700

    Change image size

--- template-parts/party.php
+++ template-parts/party.php
@@ -23,7 +23,7 @@
                 ]);
                 $teamIndex++;
                 extract($specialist);
-                $image = get_image_data($specialist_photo, 'thumbnail');
+                $image = get_image_data($specialist_photo, 'medium');
             ?>
                 <div class="party__poopup poopup hidden">
                     <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -117,7 +117,7 @@
                 ]);
                 $teamIndex++;
                 extract($specialist);
-                $image = get_image_data($specialist_photo, 'thumbnail');
+                $image = get_image_data($specialist_photo, 'medium');
             ?>
                 <div class="party__list-item">
                     <div class="party__list-item-shadow"></div>
