188ee0b27e3451bc802a0ee5b7f4eefdbc2eb178
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2025-04-07 20:30:56 +0700

    update version

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -2,11 +2,10 @@
 function ormas_pro_enqueue_assets()
 {
     // Styles
-    wp_enqueue_style('ormas-pro-style', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0.0');
-    // wp_enqueue_style('ormas-pro-slick', get_template_directory_uri() . '/assets/css/slick.css', array(), '1.0.0');
+    wp_enqueue_style('ormas-pro-style', get_template_directory_uri() . '/assets/css/main.css', array(), _S_VERSION);
 
     // Scripts
-    wp_enqueue_script('ormas-pro-script', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('ormas-pro-script', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), _S_VERSION, true);
     wp_enqueue_script('ormas-pro-slick', get_template_directory_uri() . '/assets/js/slick.js', array('jquery'), '1.0.0', true);
     wp_enqueue_script('ormas-pro-jquery-magnific-popup', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.min.js', array('jquery'), '1.1.0', true);
     wp_enqueue_script('ormas-pro-imask', get_template_directory_uri() . '/assets/js/imask.min.js', array(), '6.4.3', true);
