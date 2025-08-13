957706b5da9a03befa15ac298915786fb8c5b557
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2025-08-13 08:40:18 +0000

    removed 404 in breadcrumbs

--- functions.php
+++ functions.php
@@ -95,3 +95,19 @@ require get_template_directory() . '/inc/mail.php';
  * Settings check
  */
 require get_template_directory() . '/inc/settings-check.php';
+
+
+/* Убираем раздел Услуги в хлебных крошках */
+add_filter( 'wpseo_breadcrumb_links', function( $links ) {
+    if ( is_singular( 'service_pages' ) ) {
+        foreach ( $links as $key => $link ) {
+            if ( isset( $link['url'] ) && str_ends_with($link['url'], '/service_pages/') ) {
+                unset( $links[$key] );
+            }
+        }
+
+        // Переиндексируем массив
+        $links = array_values( $links );
+    }
+    return $links;
+});
